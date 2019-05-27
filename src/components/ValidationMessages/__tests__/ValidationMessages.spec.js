2e8dbbb163cf1d6d5f433efbe6b6d155cd4869eb
Yogesh Kubendran <Yogesh.Kubendran@isobar.com>
2019-05-27 15:57:59 +1000

    Validation Message Component added

---
+++ src/components/ValidationMessages/__tests__/ValidationMessages.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ValidationMessages from '..';
+
+describe('ValidationMessages', () => {
+  it('warning renders correctly', () => {
+    const component = mount(
+      <ValidationMessages
+        messageType="info"
+        validationMessage="Passengers has been reset to default value."
+      />
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('info message renders correctly', () => {
+    const component = mount(
+      <ValidationMessages
+        messageType="warning"
+        validationMessage="Please select a destination."
+      />
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+});
---
+++ src/components/ValidationMessages/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+const MESSAGE_TYPE = {
+  INFO: 'info',
+  WARNING: 'warning'
+};
+
+const warningMessage = ({ messageType }) => ({
+  width: '100%',
+  minHeight: '48px',
+  border: 'solid 0.5px rgba(151, 151, 151, 0)',
+  position: 'relative',
+  fontFamily: 'Ciutadella',
+  fontSize: '16px',
+  fontWeight: 'normal',
+  fontStyle: 'normal',
+  fontStretch: 'normal',
+  letterSpacing: 'normal',
+  color: '#323232',
+  padding: '11px 13px 11px 15px',
+  ...(messageType === MESSAGE_TYPE.WARNING
+    ? { backgroundColor: '#fcebcd' }
+    : { backgroundColor: '#bff4f2' })
+});
+
+const warningArrow = () => ({
+  width: 0,
+  height: 0,
+  borderLeft: '7px solid transparent',
+  borderRight: '7px solid transparent',
+  borderBottom: '7px solid #fcebcd',
+  marginLeft: '15px'
+});
+
+function ValidationMessages({ validationMessage, messageType }) {
+  return (
+    <div role="alert">
+      {messageType === MESSAGE_TYPE.WARNING && <div css={warningArrow} />}
+      <div css={warningMessage({ messageType })}>{validationMessage}</div>
+    </div>
+  );
+}
+
+ValidationMessages.propTypes = {
+  validationMessage: PropTypes.string,
+  messageType: PropTypes.string
+};
+
+ValidationMessages.defaultProps = {
+  validationMessage: '',
+  messageType: MESSAGE_TYPE.WARNING
+};
+
+export default ValidationMessages;
